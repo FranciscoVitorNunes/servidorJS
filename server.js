b73dc5ee32817997036b098470f6e155cc5af78b
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2024-11-23 16:10:34 -0300

    finalização

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-routes(app);
+app.use(express.static("uploads"))
+routes(app)
 
-app.listen(3000,() => {
-    console.log("Server is running on port 3000");
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
 });
-
-
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,30 @@
-import express from "express"
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import express from "express"; // Importa o framework Express para criar a aplicação web
+import multer from "multer"; // Importa o Multer para lidar com uploads de arquivos
+import { listarPosts, postarNovoPost, uploadImagem, atualizaNovoPost } from "../controllers/postsController.js"; // Importa as funções controladoras para lidar com a lógica dos posts
+import cors from "cors"; // Importa o CORS para lidar com requisições de origens diferentes
 
-const routes = (app) =>{
-    app.use(express.json());
-    app.get("/posts", listarPosts);
-
-    app.post("/posts", postarNovoPost)
+const corsOpitons = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200
 }
+// Cria uma instância do middleware Multer
+const upload = multer({ dest : "./uploads" });
+
+// Define as rotas usando o objeto Express app
+const routes = (app) => {
+  // Permite que o servidor interprete corpos de requisições no formato JSON
+  app.use(express.json());
+  app.use(cors(corsOpitons))
+  // Rota para recuperar uma lista de todos os posts
+  app.get("/posts", listarPosts); // Chama a função controladora apropriada
+
+  // Rota para criar um novo post
+  app.post("/posts", postarNovoPost); // Chama a função controladora para criação de posts
+
+  // Rota para upload de imagens (assumindo uma única imagem chamada "imagem")
+  app.post("/upload", upload.single("imagem"), uploadImagem); // Chama a função controladora para processamento da imagem
+
+  app.put("/upload/:id", atualizaNovoPost)
+};
 
 export default routes;
